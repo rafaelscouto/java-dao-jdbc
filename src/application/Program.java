22541ca63000754e2afdddd49839c06a113038c0
Rafael Couto <rafaelscouto@gmail.com>
2022-12-28 19:23:29 -0300

    Create Department class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,12 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Department obj = new Department(1, "Cars");
+		System.out.print(obj);
 	}
 }
