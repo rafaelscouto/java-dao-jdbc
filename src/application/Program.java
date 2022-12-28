9c74d131d57e5252e1095ea671cae40a46914798
Rafael Couto <rafaelscouto@gmail.com>
2022-12-28 20:54:57 -0300

    Create Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Cars");
-		System.out.print(obj);
+		
+		Seller seller = new Seller(21, "Rafael", "rafaelscouto@gmail.com", new Date(), 12000.0, obj);
+		
+		System.out.print(seller);
+		
 	}
 }
