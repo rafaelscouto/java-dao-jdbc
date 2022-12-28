e816b83003a0f86851133bc2e2f127a75d926d06
Rafael Couto <rafaelscouto@gmail.com>
2022-12-28 19:11:37 -0300

    Creating project and git repository

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	}
+}
