cbc4abac52f236050858ec3c081e1b6cfaef21a7
Rafael Couto <rafaelscouto@gmail.com>
2022-12-28 22:03:03 -0300

    Create SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department obj = new Department(1, "Cars");
 		
 		Seller seller = new Seller(21, "Rafael", "rafaelscouto@gmail.com", new Date(), 12000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.print(seller);
 		
