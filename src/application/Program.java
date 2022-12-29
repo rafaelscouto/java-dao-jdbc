4df72c454d91abf0abbf8dc984cdc4bef5fad92d
Rafael Couto <rafaelscouto@gmail.com>
2022-12-28 22:25:31 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Cars");
-		
-		Seller seller = new Seller(21, "Rafael", "rafaelscouto@gmail.com", new Date(), 12000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.print(seller);
 		
